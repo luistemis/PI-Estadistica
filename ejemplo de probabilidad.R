0840c2bc5821121ca69351109312798276977b46
luistemis <100747640+luistemis@users.noreply.github.com>
2022-03-23 09:21:10 -0600

    ejemplo de probabilidad
    código de una unas bolas al azar

---
+++ ejemplo de probabilidad.R
@@ -0,0 +1,11 @@
+#Se sacará una bola al azar de una caja que contiene:
+#3 bolas cian, 5 bolas magenta y 7 bolas amarillas. 
+#¿Cuál es la probabilidad de que la bola sea cian?
+cyan <- 3
+magenta <- 5
+yellow <- 7
+balls <- cyan + magenta + yellow
+p_cyan <- cyan/balls
+p_cyan
+
+## [1] 0.2
