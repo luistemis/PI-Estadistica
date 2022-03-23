42a15d89bd439315b77a9ac1d52f09d8096379e8
luistemis <100747640+luistemis@users.noreply.github.com>
2022-03-23 11:05:49 -0600

    DIISTRIBUCIÓN
    para pasarlo a markdow

---
+++ DISTRIBUCIÓN DE LOS NIÑOS.R
@@ -0,0 +1,23 @@
+
+
+
+# Probabilidad de que 35 alumnos lleguen en
+#15 minutos a la entrada de la escuela
+# x= número de alumnos
+# lamda= tiempo de llegada.
+
+x=35
+lamda=15
+#LA PROBABILIDAD ES 4.310....
+dpois(x,lamda)
+
+# Probabilidad de que 50 alumnos lleguen en
+#15 minutos a la entrada de la escuela
+# x= número de alumnos
+# lamda= tiempo de llegada.
+
+x=50
+lamda=15
+#LA PROBABILIDAD ES DE 6.41...
+
+dpois(x,lamda)
